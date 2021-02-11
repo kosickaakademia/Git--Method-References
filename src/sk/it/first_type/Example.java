db897ba887771ef78e80f1bd882c2b8d95f2ab18
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-11 13:32:34 +0100

    Update Example.java
    changed

--- src/sk/it/first_type/Example.java
+++ src/sk/it/first_type/Example.java
@@ -5,19 +5,20 @@ public class Example {
         Example example = new Example();
         MyInterface myInterface = example::calculateMax;
 
-        myInterface.execute(10, 85);
+        // changed
+        myInterface.execute(100, 850);
     }
 
     public void calculateMax(int reps, int weight) {
         switch (reps) {
             case 10:
-                System.out.println("Your max is: " + weight * (1 / 3f + 1));
+                System.out.println("Your   max is: " + weight * (1 / 3f + 1));
                 break;
             case 9:
-                System.out.println("Your max is: " + weight * (1 / 4f + 1));
+                System.out.println("Your   max is: " + weight * (1 / 4f + 1));
                 break;
             case 8:
-                System.out.println("Your max is: " + weight * (1 / 5f + 1));
+                System.out.println("Your   max is: " + weight * (1 / 5f + 1));
                 break;
         }
     }
